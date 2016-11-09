3500a9bffd0c7746679cae0b6734060012165562
Robert Main <lenswipe@gmail.com>
2016-11-09 00:30:53 +0000

    Created eloquent models for application

---
+++ app/Models/Breed.php
@@ -0,0 +1,19 @@
+<?php 
+
+namespace Furbook\Models\Breed
+
+use Illuminate\Database\Eloquent\Model;
+
+class Breed extends Model {
+	
+	/**
+     * Enable or disable table timestamps
+     *
+     * @var boolean
+     */
+	protected $timestamps = false;
+
+	public function breed(){
+		return $this->hasMany('Furbook\Models\Cat');
+	}
+}
---
+++ app/Models/Cat.php
@@ -0,0 +1,21 @@
+<?php 
+
+namespace Furbook\Models\Cat
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cat extends Model {
+	
+	/**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+	protected $fillable = [
+		'name', 'date_of_birth', 'breed_id'
+	];
+
+	public function breed(){
+		return $this->belongsTo('Furbook\Models\Breed');
+	}
+}
