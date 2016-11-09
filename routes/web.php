5cadb37c9e597b77f795b53c3a8e5948d1db6f49
Robert Main <lenswipe@gmail.com>
2016-11-09 00:02:24 +0000

    Passing variables into views

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,7 @@ Route::get('cats', function(){
 Route::get('cats/{id}', function($id){
 	return sprintf('Cat #%s', $id);
 })->where('id', '[0-9]+');
+
+Route::get('about', function(){
+	return view('about')->with('number_of_cats', 9000);
+});
