12df0835ad57ecfa71719cca1d2ae6f2f299c679
Robert Main <lenswipe@gmail.com>
2016-11-20 17:46:43 +0000

    Created overview page

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,16 @@ Route::get('cats/{id}', function($id){
 	return sprintf('Cat #%s', $id);
 })->where('id', '[0-9]+');
 
+Route::get('cats/breeds/{name}', function($name){
+	$breed = Furbook\Breed::with('cats')
+		->whereName($name)
+		->first();
+
+	return view('partials.gcats.index')
+		->with('breed', $breed)
+		->with('cats', $breed->cats);
+});
+
 Route::get('about', function(){
 	return view('partials.about')->with('number_of_cats', 9000);
 });
