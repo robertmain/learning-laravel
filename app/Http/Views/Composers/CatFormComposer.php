efbd40ec4efd3b05a24777d022401801b0eed1f6
Robert Main <lenswipe@gmail.com>
2016-12-05 01:40:25 +0000

    Application Service Providers
    Use application service providers in conjunction with view composers to
    provide an associative array of breed IDs to breed names in the view. This
    can then be used to populate the `<select>` element on the cat create/edit
    form.

---
+++ app/Http/Views/Composers/CatFormComposer.php
@@ -0,0 +1,19 @@
+<?php
+namespace Furbook\Http\Views\Composers;
+
+use Furbook\Breed;
+use Illuminate\Contracts\View\View;
+
+/**
+* Cat Form View Composer
+*
+* Provides the cat form with a list of breeds to populate a select list with
+*/
+class CatFormComposer
+{
+    
+    public function __construct(View $view)
+    {
+        $view->with('breeds', $this->breeds->lists('name', 'id'));
+    }
+}
